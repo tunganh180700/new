fee5712f80500409ee3eda19bbb00d0c3bf1afca
Luong Tan Dat <leconghung230503@gmail.com>
2026-03-09 09:43:11 +0700

    pushBase

---
+++ my-app/src/components/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">Login Page</h1>
+        </div>
+    );
+}
+export default Login;   
+ 
